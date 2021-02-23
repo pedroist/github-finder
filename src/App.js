ad11972202e42375e7f065222d74218915591102
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2021-02-23 12:15:33 +0000

    Cleaned up uneeded stuff

--- src/App.js
+++ src/App.js
@@ -3,13 +3,9 @@ import "./App.css";
 
 class App extends Component {
   render() {
-    const name = "John Doe";
-    const loading = false;
-    const showName = true;
-
     return (
       <div className='App'>
-        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
+        <h1>Hello</h1>
       </div>
     );
   }
