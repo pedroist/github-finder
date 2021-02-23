7c085f9588c04267f04fca51ff0aa2de4b699cf6
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2021-02-23 12:47:39 +0000

    imported the font-awesome in the index.js

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,7 +4,10 @@ export class Navbar extends Component {
   render() {
     return (
       <nav className='navbar bg-primary'>
-        <h1>Navbar</h1>
+        <h1>
+          <i className='fab fa-github' />
+          Navbar
+        </h1>
       </nav>
     );
   }
