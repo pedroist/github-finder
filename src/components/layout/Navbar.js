a8f9cdc6e44d6854fdc38ea7096ba09438cd4ee7
Pedro Castro <pedro.tiago.castro.ist@gmail.com>
2021-03-04 23:19:36 +0000

    Added Links to Navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const Navbar = ({ icon, title }) => {
@@ -7,18 +8,22 @@ const Navbar = ({ icon, title }) => {
       <h1>
         <i className={icon} /> {title}
       </h1>
+      <ul>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+      </ul>
     </nav>
   );
 };
 
 Navbar.defaultProps = {
   title: 'Github Finder',
-  icon: 'fab fa-github',
+  icon: 'fab fa-github'
 };
 
 Navbar.propTypes = {
   title: PropTypes.string.isRequired,
-  icon: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
 };
 
 export default Navbar;
