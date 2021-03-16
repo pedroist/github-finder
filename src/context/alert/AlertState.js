a85a1cf54cef35f54ad34460875851e4e6026472
Pedro Castro <peti@netlight.com>
2021-03-16 17:55:21 +0100

    Moved alert state to context

--- src/context/alert/AlertState.js
+++ src/context/alert/AlertState.js
@@ -1,21 +1,32 @@
-import React, { userReducer } from 'react';
+import React, { useReducer } from 'react';
 import AlertContext from './alertContext';
 import AlertReducer from './alertReducer';
 import { SET_ALERT, REMOVE_ALERT } from '../types';
 
-const AlertState = () => {
-  const initialState = {
-    alert: null
-  };
+const AlertState = props => {
+  const initialState = null;
 
   const [state, dispatch] = useReducer(AlertReducer, initialState);
 
   // Set Alert
+  const setAlert = (msg, type) => {
+    dispatch({
+      type: SET_ALERT,
+      payload: { msg, type }
+    });
 
-  // Remove Alert
+    setTimeout(() => dispatch({ type: REMOVE_ALERT }), 3000);
+  };
 
   return (
-    <AlertContext.Provider value={{}}>{props.children}</AlertContext.Provider>
+    <AlertContext.Provider
+      value={{
+        alert: state,
+        setAlert
+      }}
+    >
+      {props.children}
+    </AlertContext.Provider>
   );
 };
 
