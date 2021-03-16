6ed4ebc96a23bca67a8c7e14b2102111e3f90d55
Pedro Castro <peti@netlight.com>
2021-03-16 18:13:51 +0100

    Added NotFound page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 import User from './components/users/User';
 import About from './components/pages/About';
 import Alert from './components/layout/Alert';
@@ -23,6 +24,7 @@ const App = () => {
                 <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
